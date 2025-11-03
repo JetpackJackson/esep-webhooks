b80a58df097851f2ec3e1cb21cb59067e37b121b
JetpackJackson <jetpackjackson@katzenmue.de>
2025-11-03 15:58:07 -0500

    first

--- main.py
+++ main.py
@@ -12,18 +12,19 @@ def lambda_handler(event, context):
 
     slack_url = os.environ.get("SLACK_URL")
     # https://docs.python.org/3/library/http.client.html
-    url_parts = slack_url.split("://")
-    host_path = url_parts[1].split("/", 1)
-    host = host_path[0]
-    if len(host_path > 1):
-        path = "/" + host_path[1]
-    else:
-        path = "/"
+    # url_parts = slack_url.split("://")
+    # host_path = url_parts[1].split("/", 1)
+    # host = host_path[0]
+    # if len(host_path > 1):
+    #     path = "/" + host_path[1]
+    # else:
+    #     path = "/"
 
-    conn = http.client.HTTPSConnection(host)
+    # conn = http.client.HTTPSConnection(host)
+    conn = http.client.HTTPSConnection(slack_url)
     headers = {"Content-type": "application/json"}
 
-    conn.request("POST", path, payload, headers)
+    conn.request("POST", slack_url, payload, headers)
     response = conn.getresponse()
     data = response.read().decode("utf-8")
 
